fb8f13cce8bcd7d33c52cad89bcbdfcef0c6645d
Benedict Florance <benedictflorance.a@gmail.com>
2017-06-11 22:14:47 +0530

    Key handling added

--- arrow.js
+++ arrow.js
@@ -14,6 +14,29 @@ var sPressed = false;
 var space=false;
 var lives=10;
 var score=0;
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e) {
+    if(e.keyCode == 87) {
+        wPressed = true;
+    }
+    else if(e.keyCode == 83) {
+        sPressed = true;
+    }
+    else if(e.keyCode==32){
+        space = true;
+    }
+}
+
+function keyUpHandler(e) {
+    if(e.keyCode == 87) {
+        wPressed = false;
+    }
+    else if(e.keyCode == 83) {
+        sPressed = false;
+    }
+}
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.globalCompositeOperation ='destination-over';
